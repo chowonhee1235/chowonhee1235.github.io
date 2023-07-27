37312cf574fb9d1ea8ae300c77cf6b5eb3c19085
chowonhee1235 <85092226+chowonhee1235@users.noreply.github.com>
2023-07-27 20:01:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,28 @@
-<script src="https://apis.google.com/js/api.js"></script>
-<script>
-  /**
-   * Sample JavaScript code for youtube.liveChatMessages.list
-   * See instructions for running APIs Explorer code samples locally:
-   * https://developers.google.com/explorer-help/code-samples#javascript
-   */
+const { LiveChat } = require("youtube-chat");
 
-  function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    gapi.client.setApiKey("YOUR_API_KEY");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return gapi.client.youtube.liveChatMessages.list({
-      "liveChatId": "KicKGFVDZG1YeEJteU5RVnlfVXRfSHRNLXdOdxILeWxYMGY0Yi1OcW8",
-      "part": [
-        "snippet"
-      ]
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
-  });
-</script>
-<button onclick="authenticate().then(loadClient)">authorize and load</button>
-<button onclick="execute()">execute</button>
+const liveChat = new LiveChat({liveId: "ylX0f4b-Nqo"})
+
+const ok = liveChat.start()
+// Emit at start of observation chat.
+// liveId: string
+liveChat.on("start", (liveId) => {
+  console.log(`live start ${liveId}`);
+})
+
+// Emit at end of observation chat.
+// reason: string?
+liveChat.on("end", (reason) => {
+  console.log(`live end ${reason}`);
+})
+
+// Emit at receive chat.
+// chat: ChatItem
+liveChat.on("chat", (chatItem) => {
+  console.log(chatItem);
+})
+
+// Emit when an error occurs
+// err: Error or any
+liveChat.on("error", (err) => {
+  console.log(`error ${err}`);
+})
